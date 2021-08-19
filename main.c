8174c8fd5dc822c5a7058cac800eaf6e525b5c87
Yohannes251 <yohannes.z251@gmail.com>
2021-08-18 17:26:19 -0700

    Updated main.c

--- main.c
+++ main.c
@@ -1,18 +1,42 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include<sys/wait.h>
 
-int main(int ac, char **ar)
+int main(void)
 {
-	size_t line = 50;
-	char *str = (char *)malloc(line * sizeof(char));
-	ssize_t linelen;
+        size_t line = 50;
+        char *str;
+        str = malloc(line * sizeof(char));
+        ssize_t linelen;
 
-	while(1)
-	{
+        do{
                 printf("%s", "$ ");
+                if (stdin == NULL)
+                        break;
                 linelen = getline(&str , &line, stdin);
-                printf("%s", str);
-        }
+
+                if (linelen == '\0')
+                        return(0);
+                int i;
+                char stri[50];
+
+                for(i = 0; i < linelen - 1; i++)
+                {
+                        stri[i] = str[i];
+                }
+                stri[i++] = '\0';
+
+                char *argv[] = {stri, NULL};
+                int pid = fork();
+
+                if (pid == 0)
+                {
+                        execve(argv[0], argv, NULL);
+                        perror("Error");
+                }else {
+                        wait(NULL);
+                }
+        }while(1);
         return(0);
 }
