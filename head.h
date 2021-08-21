3434572064181302680b77e00b0fde3de00e29fc
Yohannes251 <yohannes.z251@gmail.com>
2021-08-21 18:33:46 +0000

    Updated header file

--- head.h
+++ head.h
@@ -1,5 +1,19 @@
 #ifndef HEAD_H
 #define HEAD_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <linux/limits.h>
+
 ssize_t _getline(char **buffer, size_t *n, FILE *stream);
 void prompt(void);
+
 #endif /*HEAD_H*/
